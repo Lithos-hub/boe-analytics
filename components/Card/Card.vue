73583a0e34b62de408b2af7989b83e8ee29cb83f
Lithos-hub <lithos.contact@gmail.com>
2025-01-06 17:09:24 +0100

    create initial version of the card component

---
+++ components/Card/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Card">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.Card {
+  @apply p-5 rounded-lg bg-[#101010] border border-t-[#202020] border-r-[#070707] border-b-[#050505] border-l-[#101010] bg-gradient-to-br from-[#152020] to-[#000505];
+
+  box-shadow: 31px 31px 62px #010209, -31px -31px 62px #030a25;
+}
+</style>
