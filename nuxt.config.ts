843905c222182d90f994dbc3c4cd0935769e7ef7
Carlos Segura Garcia <lithos.contact@gmail.com>
2025-01-07 13:21:54 +0100

    change favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
-})
+  modules: ["@nuxt/ui"],
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/svg+xml", href: "/logo.svg" }],
+    },
+  },
+});
