4f85b98cb3bf042805aa83fb8274f8991aa04bd8
Lithos-hub <lithos.contact@gmail.com>
2025-01-07 20:37:41 +0100

    improve layout in app.vue

--- app.vue
+++ app.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="text-white/80">
     <Navbar />
-    <Container>
-      <NuxtPage />
+    <Container class="overflow-y-auto max-h-[calc(100vh-56px)]">
+      <div class="max-w-[95vw] md:w-[80vw] mx-auto py-10">
+        <NuxtPage />
+      </div>
     </Container>
 
     <PatternBackground />
-    <!-- <BlurBackground /> -->
   </div>
 </template>
+
+<style>
+html,
+body {
+  @apply overflow-hidden;
+}
+
+/* Custom scrollbar styles */
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+::-webkit-scrollbar-track {
+  @apply bg-stone-800;
+}
+
+::-webkit-scrollbar-thumb {
+  @apply bg-primary/50 rounded-lg;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  @apply bg-gray-500/50;
+}
+</style>
