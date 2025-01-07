e2f45a60a3a23819e71ca70b1b7b5c03979316af
Carlos Segura Garcia <lithos.contact@gmail.com>
2025-01-07 13:24:49 +0100

    fix navbar position

--- components/Navbar/Navbar.vue
+++ components/Navbar/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
-  <header>
+  <header class="fixed top-0 left-0 w-full backdrop-blur-xl">
     <nav
       class="px-5 h-14 flex justify-between border-b items-center border-primary/10"
     >
       <div class="flex gap-5 items-center">
         <NuxtLink to="/" class="flex gap-2.5">
           <img src="/logo.svg" width="25" />
-          <strong class="text-primary">BOE Resumido</strong>
+          <span class="text-primary">BOE RESUMIDO</span>
         </NuxtLink>
       </div>
       <NuxtLink to="/about" class="text-primary">Sobre el proyecto</NuxtLink>
