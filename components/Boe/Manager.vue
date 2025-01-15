5a867a909fbd14190782ccd2f6d4d147c2afd577
Lithos-hub <lithos.contact@gmail.com>
2025-01-15 17:20:51 +0100

    feat(Manager): use setup function instead script setup

--- components/Boe/Manager.vue
+++ components/Boe/Manager.vue
@@ -1,89 +1,97 @@
 <template>
   <div class="BoeManager__wrapper">
     <section class="flex w-full flex-wrap items-stretch justify-center gap-5">
-      <article class="w-full flex-1">
+      <article class="flex-1">
         <Card class="relative flex h-full flex-col justify-between">
           <BoeSummary
-            :text="processedBoeData?.briefSummary ?? ''"
+            :text="summaryAndStatsData?.briefSummary ?? ''"
             :boe-date-raw="boeDateRaw"
             :boe-date="boeDate"
             :boe-link="scrappedBoeData?.link ?? ''" />
         </Card>
       </article>
-      <article class="flex grow-0 flex-col">
+      <article class="grow-0 flex-col">
         <Card class="h-full">
           <BoeStats
-            :stats="
-              processedBoeData?.stats ?? {
-                positive: 0,
-                negative: 0,
-                neutral: 0,
-              }
-            " />
+            v-if="summaryAndStatsData"
+            :stats="summaryAndStatsData?.stats" />
         </Card>
       </article>
     </section>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
 import type { BoeScrapingResponse, BoeSummaryResponse } from './Boe.interfaces';
 
-const scrappedBoeData = ref<BoeScrapingResponse | null>(null);
-const processedBoeData = ref<BoeSummaryResponse | null>(null);
+export default defineComponent({
+  async setup() {
+    const scrappedBoeData = ref<BoeScrapingResponse | null>(null);
+    const summaryAndStatsData = ref<BoeSummaryResponse | null>(null);
 
-// boeDate is the date in the format 'lunes 6 de enero de 2025'
-const boeDate = new Date().toLocaleDateString('es-ES', {
-  weekday: 'long',
-  day: 'numeric',
-  month: 'long',
-  year: 'numeric',
-});
+    // boeDate is the date in the format 'lunes 6 de enero de 2025'
+    const boeDate = new Date().toLocaleDateString('es-ES', {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    });
 
-// boeDateRaw is the date in the format 'YYYY-MM-DD'
-const boeDateRaw = new Date().toISOString().split('T')[0];
+    // boeDateRaw is the date in the format 'YYYY-MM-DD'
+    const boeDateRaw = new Date().toISOString().split('T')[0];
 
-const currentYear = String(new Date().getFullYear());
-const currentMonth = String(new Date().getMonth() + 1);
-const currentDay = String(new Date().getDate());
+    const currentYear = String(new Date().getFullYear());
+    const currentMonth = String(new Date().getMonth() + 1);
+    const currentDay = String(new Date().getDate());
 
-// The formattedDate format must be 'YYYY-MM-DD'
-const formattedDate = `${currentYear}-${currentMonth.padStart(2, '0')}-${currentDay.padStart(2, '0')}`;
+    // The formattedDate format must be 'YYYY-MM-DD'
+    const formattedDate = `${currentYear}-${currentMonth.padStart(2, '0')}-${currentDay.padStart(2, '0')}`;
 
-const getBoeData = async () => {
-  const { data: boeData, error } = await useFetch<BoeScrapingResponse>(
-    `/api/scrap/${formattedDate}`,
-  );
+    const getBoeData = async () => {
+      try {
+        const { data: boeScrapData, error } =
+          await useFetch<BoeScrapingResponse>(`/api/scrap/${formattedDate}`, {
+            onResponseError({ response }) {
+              console.error('Response error:', response._data);
+            },
+            onRequestError({ error }) {
+              console.error('Request error:', error);
+            },
+          });
 
-  scrappedBoeData.value = boeData.value;
+        if (error.value) {
+          console.error('Error scraping BOE data:', error.value);
+          return;
+        }
 
-  if (error.value?.statusCode === 404) {
-    processedBoeData.value = {
-      briefSummary:
-        'El BOE de hoy no está disponible. Inténtalo más tarde o consulta el BOE de otra fecha usando el calendario.',
-      stats: {
-        positive: 0,
-        negative: 0,
-        neutral: 0,
-      },
+        scrappedBoeData.value = boeScrapData.value;
+
+        const { data } = await useFetch<BoeSummaryResponse>(
+          `/api/boe/summary`,
+          {
+            method: 'POST',
+            body: {
+              text: boeScrapData.value?.text ?? '',
+            },
+          },
+        );
+
+        summaryAndStatsData.value = data.value;
+      } catch (e) {
+        console.error('Error in getBoeData:', e);
+      }
     };
-  } else {
-    const { data: processedBoeData } = await useFetch<BoeSummaryResponse>(
-      `/api/boe/summary`,
-      {
-        method: 'POST',
-        body: {
-          text: boeData.value?.text ?? '',
-        },
-      },
-    );
 
-    processedBoeData.value = processedBoeData.value;
-  }
-};
+    await getBoeData();
 
-onMounted(() => {
-  getBoeData();
+    return {
+      scrappedBoeData,
+      summaryAndStatsData,
+      boeDate,
+      boeDateRaw,
+    };
+  },
 });
 </script>
 
