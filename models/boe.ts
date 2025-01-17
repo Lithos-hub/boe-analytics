6e350fc02aeca3023758dbc6ce748788e72a2400
Lithos-hub <lithos.contact@gmail.com>
2025-01-17 09:51:03 +0100

    feat(models): create Boe models interfaces

---
+++ models/boe.ts
@@ -0,0 +1,93 @@
+/**
+
+Table boes
+
+id (Primary Key, Auto Increment)
+date (Date, Unique) - The date of the analyzed BOE.
+url (Text) - The url of the BOE.
+created_at (Timestamp) - The timestamp when the analysis was generated.
+
+Table summaries
+
+id (Primary Key, Auto Increment)
+boe_id (Foreign Key) - References boes.id.
+summary (Text) - The summary of the BOE.
+
+Table areas
+
+id (Primary Key, Auto Increment)
+boe_id (Foreign Key) - References boes.id.
+name (Text) - The name of the area.
+description (Text) - The description of the area.
+
+Table statistics
+
+id (Primary Key, Auto Increment)
+boe_id (Foreign Key) - References boes.id.
+type (Enum: 'positive', 'negative', 'neutral') - Type of statistic.
+count (Integer) - The count of positive, negative, or neutral aspects.
+
+Table main_points
+
+id (Primary Key, Auto Increment)
+boe_id (Foreign Key) - References boes.id.
+point (Text) - A main point from the BOE.
+
+Table keywords
+
+id (Primary Key, Auto Increment)
+boe_id (Foreign Key) - References boes.id.
+keyword (Text) - A keyword from the BOE.
+
+Table aspects
+
+id (Primary Key, Auto Increment)
+boe_id (Foreign Key) - References boes.id.
+type (Enum: 'positive', 'negative', 'neutral') - Type of aspect.
+aspect (Text) - A positive, negative, or neutral aspect from the BOE.
+
+ */
+
+export interface BoeColumn {
+  id: number;
+  boe_id: number;
+}
+
+export interface Boe {
+  id: number;
+  date: string;
+  url: string;
+}
+
+export interface Area extends BoeColumn {
+  name: string;
+  description: string;
+}
+
+export interface Statistic extends BoeColumn {
+  type: string;
+  count: number;
+}
+
+export interface MainPoint extends BoeColumn {
+  point: string;
+}
+
+export interface Keyword extends BoeColumn {
+  keyword: string;
+}
+
+export interface Aspect extends BoeColumn {
+  type: string;
+  aspect: string;
+}
+
+export interface BoeResponse {
+  id: number;
+  date: string;
+  summary: string | null;
+  stats: Statistic[] | null;
+  main_points: MainPoint[] | null;
+  keywords: Keyword[] | null;
+  aspects: Aspect[] | null;
+}
