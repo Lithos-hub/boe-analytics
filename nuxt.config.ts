164faa66aa7eead25735400f5ec10db64dccf782
Lithos-hub <lithos.contact@gmail.com>
2025-01-17 09:49:44 +0100

    feat(nuxt): config supabase and env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,4 +24,13 @@ export default defineNuxtConfig({
   tailwindcss: {
     exposeConfig: true,
   },
+  supabase: {
+    redirect: false,
+  },
+  runtimeConfig: {
+    public: {
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      SUPABASE_KEY: process.env.SUPABASE_KEY,
+    },
+  },
 });
