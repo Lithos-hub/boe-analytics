479f8019ff34a5550be1df661cefc2967b295776
Carlos Segura Garcia <lithos.contact@gmail.com>
2025-01-21 13:58:26 +0100

    feat(summary): improve prompt

--- server/services/openai/summary.ts
+++ server/services/openai/summary.ts
@@ -5,7 +5,7 @@ export const getSummary = async (text: string) => {
     messages: [
       {
         role: 'system',
-        content: `Se te proporcionará un texto relativo al Boletín Oficial del Estado de España. Necesito que elabores un resumen del mismo en formato de texto en un par de párrafos. Separa cada párrafo con un salto de línea. Necesito que uses un lenguaje claro y que sea entendible para la mayoría de la población sin necesidad de conocimientos técnicos.
+        content: `Se te proporcionará un texto relativo al Boletín Oficial del Estado de España. Necesito que elabores un resumen del mismo en dos o tres párrafos. Separa cada párrafo con un salto de línea, en cuenta que el texto será mostrado en una web con HTML. Necesito que uses un lenguaje claro y que sea entendible para la mayoría de la población sin necesidad de conocimientos técnicos.
           
           El texto es el siguiente:
           ${text}`,
