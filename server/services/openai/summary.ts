01bc94f6480380ed2528f533c44e2132ea5a155e
Carlos Segura Garcia <lithos.contact@gmail.com>
2025-01-22 09:17:12 +0100

    feat(openai): improve summary prompt

--- server/services/openai/summary.ts
+++ server/services/openai/summary.ts
@@ -6,6 +6,10 @@ export const getSummary = async (text: string) => {
       {
         role: 'system',
         content: `Se te proporcionará un texto relativo al Boletín Oficial del Estado de España. Necesito que elabores un resumen del mismo en dos o tres párrafos. Separa cada párrafo con un salto de línea, en cuenta que el texto será mostrado en una web con HTML. Necesito que uses un lenguaje claro y que sea entendible para la mayoría de la población sin necesidad de conocimientos técnicos.
+
+        Ejemplo de salida:
+
+        "Este es el párrafo 1.\n\nEste es el párrafo 2.\n\nEste es el párrafo 3."
           
           El texto es el siguiente:
           ${text}`,
