fff4c57ded253ad9a37c1f5e6c77096de500cbcd
Carlos Segura Garcia <lithos.contact@gmail.com>
2025-01-27 10:03:34 +0100

    feat(navbar): add mobile menu

--- components/Navbar/Navbar.vue
+++ components/Navbar/Navbar.vue
@@ -11,7 +11,7 @@
           <span class="Navbar__logo-text">BOE Analytics</span>
         </NuxtLink>
       </div>
-      <div class="flex gap-5">
+      <div class="hidden gap-5 md:flex">
         <ULink
           :to="`/${dateRaw}`"
           active-class="text-primary font-bold underline underline-offset-4"
@@ -25,14 +25,59 @@
           Sobre el proyecto
         </ULink>
       </div>
+      <div class="flex md:hidden">
+        <UButton icon="i-heroicons-bars-3" variant="soft" @click="showMenu" />
+      </div>
     </nav>
   </header>
+  <USlideover v-model="isShowingMenu">
+    <div class="flex-1 p-4">
+      <UButton
+        color="gray"
+        variant="ghost"
+        icon="i-heroicons-x-mark-20-solid"
+        class="absolute end-5 top-5 z-10 flex sm:hidden"
+        square
+        padded
+        @click="closeMenu" />
+    </div>
+    <div class="flex h-full flex-col items-center justify-center gap-5">
+      <ULink
+        :to="`/${dateRaw}`"
+        active-class="text-primary font-bold underline underline-offset-4"
+        inactive-class="text-gray-500">
+        BOE de hoy
+      </ULink>
+      <ULink
+        to="/about"
+        active-class="text-primary font-bold underline underline-offset-4"
+        inactive-class="text-gray-500">
+        Sobre el proyecto
+      </ULink>
+    </div>
+  </USlideover>
 </template>
 
 <script setup lang="ts">
 import { getCurrentDate } from '@/utils/dates';
 
+const route = useRoute();
+
 const { dateRaw } = getCurrentDate();
+
+const isShowingMenu = ref(false);
+
+const showMenu = () => {
+  isShowingMenu.value = !isShowingMenu.value;
+};
+
+const closeMenu = () => {
+  isShowingMenu.value = false;
+};
+
+watch(route, () => {
+  closeMenu();
+});
 </script>
 
 <style lang="scss" scoped>
