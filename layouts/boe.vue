48b38c4a8b21fcb9420dc7537029b7e328c14c7b
Carlos Segura Garcia <lithos.contact@gmail.com>
2025-01-27 09:35:39 +0100

    feat(pdf-lib): install lib and use it in a initial PDF version

--- layouts/boe.vue
+++ layouts/boe.vue
@@ -20,8 +20,7 @@
             variant="soft"
             class="border border-green-500/50"
             icon="i-heroicons-arrow-down-tray"
-            disabled
-            @click="() => {}">
+            @click="downloadPDF">
             Descargar PDF
           </UButton>
           <UButton
@@ -49,9 +48,129 @@
 </template>
 
 <script setup lang="ts">
-const { isShowingJSON, boeUrl } = storeToRefs(useBoeStore());
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
+
+const { isShowingJSON, boeUrl, summary, mainPoints, keywords, areas, aspects } =
+  storeToRefs(useBoeStore());
 
 const toggleJSON = () => {
   isShowingJSON.value = !isShowingJSON.value;
+};
+
+const downloadPDF = async () => {
+  const pdfDoc = await PDFDocument.create();
+
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+  const page = pdfDoc.addPage([600, 800]);
+
+  const { width, height } = page.getSize();
+  const margin = 50;
+  let yPosition = height - margin;
+
+  const drawTextBlock = (
+    title,
+    content,
+    fontSizeTitle,
+    fontSizeContent,
+    spacing,
+  ) => {
+    page.drawText(title, {
+      x: margin,
+      y: yPosition,
+      size: fontSizeTitle,
+      font: helveticaFont,
+      color: rgb(99, 102, 241),
+    });
+    yPosition -= spacing;
+
+    page.drawText(content, {
+      x: margin,
+      y: yPosition,
+      size: fontSizeContent,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+    yPosition -= spacing + content.split('\n').length * fontSizeContent;
+  };
+
+  drawTextBlock('Resumen', summary.value, 18, 12, 40);
+  drawTextBlock(
+    'Puntos principales',
+    mainPoints.value.join('\n\n'),
+    18,
+    12,
+    40,
+  );
+  drawTextBlock('Palabras clave', keywords.value.join(', '), 18, 12, 40);
+
+  page.drawText('Áreas', {
+    x: margin,
+    y: yPosition,
+    size: 18,
+    font: helveticaFont,
+    color: rgb(0, 0, 0),
+  });
+  yPosition -= 40;
+
+  areas.value.forEach(({ name, description }, index) => {
+    page.drawText(`${index + 1}. ${name}`, {
+      x: margin,
+      y: yPosition,
+      size: 12,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+    yPosition -= 20;
+
+    page.drawText(description, {
+      x: margin,
+      y: yPosition,
+      size: 10,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+    yPosition -= 40;
+  });
+
+  page.drawText('Aspectos', {
+    x: margin,
+    y: yPosition,
+    size: 18,
+    font: helveticaFont,
+    color: rgb(0, 0, 0),
+  });
+  yPosition -= 40;
+
+  aspects.value.forEach(({ aspect, type, description }, index) => {
+    page.drawText(`${index + 1}. ${aspect} (${type})`, {
+      x: margin,
+      y: yPosition,
+      size: 12,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+    yPosition -= 20;
+
+    page.drawText(description, {
+      x: margin,
+      y: yPosition,
+      size: 10,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+    yPosition -= 40;
+  });
+  const pdfBytes = await pdfDoc.save();
+
+  const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+
+  const link = document.createElement('a');
+
+  link.href = URL.createObjectURL(blob);
+
+  link.download = `${boeUrl.value.split('id=')[1]}.pdf`;
+
+  link.click();
 };
 </script>
