07b468b0a3b14e39d88950c22bacd5f6d4a2d2e1
Carlos Segura Garcia <lithos.contact@gmail.com>
2025-01-29 07:22:26 +0100

    feat(layout): improve styles

--- layouts/boe.vue
+++ layouts/boe.vue
@@ -10,10 +10,7 @@
       </div>
     </nav>
     <div class="m-2.5 md:m-0 md:flex-1 md:p-2.5">
-      <div
-        class="max-h-[calc(100vh-100px)] overflow-y-scroll rounded-2xl bg-dark-950/50 p-5 backdrop-blur-sm">
-        <slot />
-      </div>
+      <slot />
     </div>
   </div>
 </template>
