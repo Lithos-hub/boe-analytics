cd2299d6f91e81a054119cdb166a1dae3fe08a0a
Carlos Segura Garcia <lithos.contact@gmail.com>
2025-02-15 08:13:23 +0100

    feat(about): improve info

--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,7 @@
     <img
       src="/logo-big.png"
       class="absolute left-1/2 top-1/2 h-[700px] w-[700px] -translate-x-1/2 -translate-y-1/2 object-cover opacity-10" />
-    <section class="flex flex-col items-center justify-center gap-5">
+    <section class="About__section">
       <h3 class="w-1/2 text-center text-2xl">
         Conocer y entender las leyes y medidas que se aplican a diario en el
         Boletín Oficial del Estado no es solo un derecho que tenemos como
@@ -18,7 +18,7 @@
         Boe Analytics
       </h3>
     </section>
-    <section class="About__section-explanation">
+    <section class="About__section About__section-explanation">
       <h3 class="About__title">¿Qué es BOE Analytics?</h3>
       <div class="About__description">
         <p>
@@ -40,13 +40,13 @@
         </p>
         <br />
         <p>
-          El modelo encargado de realizar esta tarea pertenece a
+          El modelo encargado de realizar el análisis pertenece a
           <strong>DeepSeek</strong>
           , un potente proyecto de Inteligencia Artificial de código abierto.
         </p>
       </div>
     </section>
-    <section class="About__section-explanation">
+    <section class="About__section About__section-explanation">
       <h3 class="About__title">¿Qué hace?</h3>
       <div class="About__description">
         <p>
@@ -89,39 +89,76 @@
     <section class="About__section-explanation">
       <h3 class="About__title">¿Cómo funciona?</h3>
       <div class="About__description">
-        <p>
-          Simplemente selecciona una fecha en el
-          <strong>calendario</strong>
-          o escríbela en la URL para generar el análisis del BOE de ese día.
-        </p>
-        <p>
-          Ten en cuenta que algunos documentos son considerablemente grandes en
-          cuanto a cantidad de palabras e información a procesar, por lo que el
-          tiempo de análisis puede variar.
-        </p>
-        <div
-          class="flex flex-wrap items-center gap-2 rounded-2xl border border-blue-500/50 bg-blue-500/10 p-5">
-          <UIcon
-            name="i-heroicons-information-circle"
-            class="h-6 w-6 text-blue-500" />
-          <p>
-            Cada BOE analizado es guardado en la base de datos
-            <strong>sin necesidad de registrar una cuenta.</strong>
-          </p>
-          <ul class="About__list">
-            <li class="About__list-item">
-              Los BOEs analizados y almacenados aparecerán con un
-              <span class="font-bold text-green-500">punto verde</span>
-              en el
-              <strong class="text-primary-500">calendario.</strong>
+        <div class="space-y-6">
+          <ol class="list-inside list-decimal space-y-8">
+            <li class="space-y-2">
+              <span class="font-semibold text-primary-500">
+                Selección de fecha
+              </span>
+              <p>
+                Simplemente selecciona una fecha en el
+                <strong>calendario</strong>
+                o escríbela en la URL para generar el análisis del BOE de ese
+                día.
+              </p>
             </li>
-            <li>
-              Los BOEs cuyo análisis no ha sido posible por algún motivo
-              aparecerán con un
-              <span class="font-bold text-red-500">punto rojo</span>
-              y serán guardados igualmente en la base de datos.
+
+            <li class="space-y-2">
+              <span class="font-semibold text-primary-500">
+                Registro visual en el calendario
+              </span>
+              <p>
+                Cuando al menos un documento ha sido analizado (total o
+                parcialmente) y registrado en base de datos, aparecerá marcado
+                en el calendario:
+              </p>
+              <img
+                src="/calendar-example.jpg"
+                class="w-[300px]"
+                alt="Ejemplo de calendario con fechas marcadas" />
             </li>
-          </ul>
+
+            <li class="space-y-2">
+              <span class="font-semibold text-primary-500">
+                Aspectos a considerar
+              </span>
+              <p>
+                Ten en cuenta que algunos documentos son considerablemente
+                grandes en cuanto a cantidad de palabras e información a
+                procesar, por lo que el tiempo de análisis puede variar.
+              </p>
+            </li>
+
+            <li class="space-y-2">
+              <span class="font-semibold text-primary-500">
+                Estado del documento
+              </span>
+              <p>
+                Al desplegar la lista de documentos, podrás ver el estado de
+                cada uno:
+              </p>
+              <img
+                src="/selector-example.jpg"
+                class="w-[500px]"
+                alt="Ejemplo de selector de documentos" />
+              <ul class="ml-4 space-y-2">
+                <li class="flex items-center">
+                  🟢
+                  <small class="ml-2">Documento completamente analizado</small>
+                </li>
+                <li class="flex items-center">
+                  🟡
+                  <small class="ml-2">
+                    Análisis parcial - Faltan algunas secciones
+                  </small>
+                </li>
+                <li class="flex items-center">
+                  🔴
+                  <small class="ml-2">Pendiente de análisis</small>
+                </li>
+              </ul>
+            </li>
+          </ol>
         </div>
       </div>
     </section>
@@ -136,13 +173,17 @@ definePageMeta({
 
 <style lang="scss" scoped>
 .About {
-  @apply relative h-[calc(100vh-150px)] snap-y snap-proximity;
+  @apply relative flex h-full flex-col gap-10;
   p {
     @apply text-center text-lg text-primary-200 md:text-left;
 
     strong {
       @apply text-primary-500;
     }
+  }
+
+  &__section {
+    @apply flex h-[calc(100vh-150px)] flex-col items-center justify-center gap-5;
   }
 
   &__title {
@@ -154,7 +195,7 @@ definePageMeta({
   }
 
   &__list {
-    @apply flex list-disc flex-col gap-2 p-5;
+    @apply flex flex-col gap-2 p-5;
 
     strong {
       @apply text-primary-500;
@@ -162,11 +203,11 @@ definePageMeta({
   }
 
   section {
-    @apply h-[calc(100vh-150px)] snap-center;
+    @apply py-10;
   }
 
   &__section-explanation {
-    @apply grid grid-cols-2 items-center justify-center;
+    @apply grid grid-cols-2 items-start justify-center;
   }
 }
 </style>
